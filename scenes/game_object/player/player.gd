0d7f2fa3e6ba596366e1c349c717728e75064e12
Bartosz Cholewa <bbcholewa@gmail.com>
2025-12-25 13:40:35 +0100

    Lesson 26 - Player Health and Damage

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -5,13 +5,17 @@ const MAX_SPEED = 125
 ## Higher is faster acceleration
 const ACCELERATION_SMOOTHING = 25
 
+@onready var damage_interval_timer = $DamageIntervalTimer
 
-# Called when the node enters the scene tree for the first time.
+var number_colliding_bodies = 0
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	$CollistionArea2D.body_entered.connect(on_body_entered)
+	$CollistionArea2D.body_exited.connect(on_body_exited)
+	damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var movement_vector = get_movement_vector()
 	var direction = movement_vector.normalized()
@@ -28,3 +32,24 @@ func get_movement_vector():
 	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	
 	return Vector2(x_movement, y_movement)
+
+
+func check_deal_damage():
+	if number_colliding_bodies == 0 || !damage_interval_timer.is_stopped():
+		return
+	
+	$HealthComponent.damage(1)
+	damage_interval_timer.start()
+	
+	print($HealthComponent.current_health)
+
+
+func on_body_entered(other_body: Node2D):
+	number_colliding_bodies += 1
+	check_deal_damage()
+	
+func on_body_exited(other_body: Node2D):
+	number_colliding_bodies -= 1
+	
+func on_damage_interval_timer_timeout():
+	check_deal_damage()

e36cdbb2b782c7991bf49120bbab053947cd3284
Bartosz Cholewa <bbcholewa@gmail.com>
2026-01-06 23:07:20 +0100

    Lesson 62 - Main Menu

---
+++ scenes/ui/main_menu.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+
+func _ready() -> void:
+	$%PlayButton.pressed.connect(on_play_pressed)
+	$%OptionsButton.pressed.connect(on_options_pressed)
+	$%QuitButton.pressed.connect(on_quit_pressed)
+	
+	
+func on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
+	
+func on_options_pressed() -> void:
+	pass
+	
+func on_quit_pressed() -> void:
+	get_tree().quit()

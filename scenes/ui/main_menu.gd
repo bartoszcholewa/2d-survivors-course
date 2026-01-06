59364fbcadb2cc0ffa21b03074a1bd4939408e42
Bartosz Cholewa <bbcholewa@gmail.com>
2026-01-06 23:45:05 +0100

    Lesson 63 - Options Menu

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -1,5 +1,7 @@
 extends CanvasLayer
 
+var options_scene: PackedScene = preload("res://scenes/ui/options_menu.tscn")
+
 
 func _ready() -> void:
 	$%PlayButton.pressed.connect(on_play_pressed)
@@ -11,7 +13,13 @@ func on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
 	
 func on_options_pressed() -> void:
-	pass
+	var options_menu_instance: Node = options_scene.instantiate()
+	add_child(options_menu_instance)
+	options_menu_instance.back_pressed.connect(on_options_back_pressed.bind(options_menu_instance))
 	
 func on_quit_pressed() -> void:
 	get_tree().quit()
+	
+	
+func on_options_back_pressed(option_instance: Node) -> void:
+	option_instance.queue_free()
